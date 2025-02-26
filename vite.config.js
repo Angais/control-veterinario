7da16446f13f5a96ec15eb5f685b36eda3434852
Angais <angelbarnesbogado@gmail.com>
2025-02-26 12:42:01 +0100

    Arreglar Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/control-veterinario/'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
