e90c0dbac3bbb917314f62fd01d4f1897380e946
Angais <angelbarnesbogado@gmail.com>
2025-02-26 12:46:13 +0100

    Cambiar Header para que concuerde con el nombre del proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
     return(
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-                Sistema de Pacientes {''}
-                <span className="text-green-500">Veterinaria</span>
+                Control {''}
+                <span className="text-green-500">Veterinario</span>
             </h1>
             <p className="text-xl mt-5 text-center">
                 Administra tus {''}
